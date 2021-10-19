5500a26a2fc9b6c5b712ef20d69de645d99cc311
Yabk <yabk1010@gmail.com>
2021-10-19 13:06:34 +0200

    Initial commit

---
+++ dz01-rjesenja.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+
+#!#!#!#!#!#!#
+# 1. Ispisite trenutni radni direktorij. (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 2. Ispisite sadrzaj .bash_history datoteke u vasem maticnom direktoriju.
+# (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 3. Ispisite sadrzaj home direktorija trenutnog korisnika sortirano po
+# velicini uzlazno. (1.0)
+
+
+
+#!#!#!#!#!#!#
+# 4. Bez mijenjanja radnog direktorija, unutar direktorija /tmp napravite
+# direktorij "OKOSL tjedan" koji ce sadrzavati direktorije ponedjeljak, utorak,
+# srijeda, cetvrtak, petak i subota, gdje ce subota biti skriveni direktorij.
+# (1.0)
+
+
+
+#!#!#!#!#!#!#
+# 5. Bez mijenjanja radnog direktorija, u skrivenom direktoriju iz proslog
+# zadatka subota napravite prazne datoteke predavanja, labosi, zadaca1, zadaca2
+# ... zadaca8. (1.0)
+
+
+
+#!#!#!#!#!#!#
+# 6. Kopirajte direktorij subota u direktorij ponedjeljak. (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 7. Ispisite cijeli sadrzaj direktorija "/tmp/OKOSL tjedan" rekurzivno kako
+# biste dokazali da su uistinu svi direktoriji i datoteke napravljeni kako smo
+# i htjeli. (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 8. U svom maticnom direktoriju stvorite simbolicku poveznicu Varionica na
+# direktorij /var. (0.75)
+
+
+
+#!#!#!#!#!#!#
+# 9. Ispisite sadrzaj /var direktorija koristeci poveznicu stvorenu u proslom
+# zadatku. (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 10. Izbrisite simbolicku poveznicu Varionica (0.5)
+
+
+
+#!#!#!#!#!#!#
+# 11. Odredite Koliko vam je preostalo slobodne memorije na disku montiranom na
+# / direktorij. Pobrinite se da je ispis u ljudski citljivom formatu. (0.75)
+
+
+
+#!#!#!#!#!#!#
+# 12. Odredite kojeg su tipa datoteke /bin/bash, /etc/passwd i /boot, tim
+# redoslijedom (0.5)
+
+
+
+#!#!#!#!#!#!#
+
